ef46f9b93d3bcc3b0a051695c0d520ff48a0fb90
Peter Vermeulen <contact@valkyriestudios.be>
2018-11-18 21:12:03 +0100

    Update outdated packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,15 @@
-const gulp = require('gulp');
+const gulp  = require('gulp');
 const babel = require('gulp-babel');
-const replace = require('gulp-replace');
-const pkg = require('./package.json');
 
 gulp.task('default', () => {
     return gulp
         .src(['src/**/*.js'])
-        .pipe(replace('__NET_VERSION__', pkg.version))
         .pipe(babel({
-            presets: ['es2015', 'stage-2'],
+            presets: [
+                '@babel/preset-env',
+            ],
             plugins : [
-                'check-es2015-constants',
-                'transform-remove-console',
-                'transform-minify-booleans',
-                'transform-property-literals',
-                'transform-member-expression-literals',
+                '@babel/plugin-proposal-class-properties',
             ]
         }))
         .pipe(gulp.dest('dist/'));
