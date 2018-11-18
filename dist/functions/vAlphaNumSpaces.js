0d08342d47770e37520259a48b58a33da010f9b0
Peter Vermeulen <contact@valkyriestudios.be>
2018-11-18 21:18:02 +0100

    Remove old dist

--- dist/functions/vAlphaNumSpaces.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-    value: !0
-});
-exports.default = vAlphaNumSpaces;
-function vAlphaNumSpaces(val) {
-    return val ? /^([a-z0-9\s])+$/igm.test(val) : !1;
-}
