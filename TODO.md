5caaca3cfce1b5f45986f3023b3250b5f7cd99a3
Peter Vermeulen <contact@valkyriestudios.be>
2019-11-11 21:07:43 +0100

    [ops] Add vIn rule and accompanying test

--- TODO.md
+++ TODO.md
@@ -1,14 +1,12 @@
 # Rules
 
 - vBetween
-- vIn
 - vRequired
 
 # Tests
 
 - vBetween
 - vEqualTo
-- vIn
 - vMax
 - vRequired
 - vSize
@@ -16,6 +14,5 @@
 # Functionality
 
 - Extend logic : Allow for a rule to be added globally and then be reused in validator instances
-- Test environment
 - Readme update documentation
 - Fill out changelog
---
+++ src/functions/vIn.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import { isString }     from '@valkyriestudios/utils/string';
+import { isBoolean }    from '@valkyriestudios/utils/boolean';
+import { isNumber }     from '@valkyriestudios/utils/number';
+import { fnv1A }        from '@valkyriestudios/utils/hash';
+
+export default function vIn (val, params) {
+    if (isString(val) || isNumber(val) || isBoolean(val)) {
+        return params.indexOf(val) > -1;
+    }
+
+    const hashed = [...params].map(el => fnv1A(el));
+    return hashed.indexOf(fnv1A(val)) > -1;
+}
