410bef8c62cf531e89740e78e0142504ecf18c8f
Peter Vermeulen <contact@valkyriestudios.be>
2021-01-06 15:00:58 +0100

    [ops] Add test for color_hex validation function

---
+++ test/src/vColorHex.test.js
@@ -0,0 +1,89 @@
+'use strict';
+
+import Validator from '../../src/index';
+
+describe("vColorHex", () => {
+    const invalids = [
+        100,
+        200,
+        'hello',
+        false,
+        true,
+        new RegExp(),
+        new Date(),
+        {},
+        [],
+        '',
+        new String('Foo'),
+        new Array(),
+        Object.create(null),
+    ];
+
+    const invalid_hex = [
+        'ABC',
+        'ABCDE',
+        'FFFFFF',
+        '0G0',
+        'FOO',
+        '0G00G0',
+        '0Z0',
+        '0Z0Z0Z',
+        '#0Z0',
+        '#FFG',
+        '#FFJJFF',
+        'Foo',
+        'Hello world',
+        'Bar',
+        'blue',
+        'red',
+        'black',
+    ];
+
+    const valids = [
+        '#000',
+        '#000000',
+        '#ABC123',
+        '#ABC456',
+        '#DEF789',
+        '#123456',
+        '#F00',
+        '#0F0',
+        '#00F',
+        '#FF00FF',
+        '#FBCDEF',
+        '#E9E9E9',
+        '#FFF',
+        '#FFFFFF',
+    ];
+
+    it ('should validate a hex color string correctly', () => {
+        const v = new Validator({a: 'color_hex'});
+
+        for (let el of valids) {
+            const evaluation = v.validate({a: el});
+            expect(evaluation.is_valid).toEqual(true);
+            expect(evaluation.errors.a).toEqual([]);
+        }
+    });
+
+    it ('should not validate other types as valid hex colors', () => {
+        const v = new Validator({a: 'color_hex'});
+
+        for (let el of invalids) {
+            const evaluation = v.validate({a: el});
+            expect(evaluation.is_valid).toEqual(false);
+            expect(evaluation.errors.a).toEqual([{msg:'color_hex', params: []}]);
+        }
+    });
+
+    it ('should not validate improper strings as valid hex colors', () => {
+        const v = new Validator({a: 'color_hex'});
+
+        for (let el of invalid_hex) {
+            const evaluation = v.validate({a: el});
+            expect(evaluation.is_valid).toEqual(false);
+            expect(evaluation.errors.a).toEqual([{msg:'color_hex', params: []}]);
+        }
+    });
+
+});
