82870b176682aa5bc7f44fa5052d413c38689327
Peter Vermeulen <contact@valkyriestudios.be>
2023-12-10 14:13:48 +0100

    [ops] Update tests for vGreaterThanOrEqual

--- test/src/functions/vGreaterThanOrEqual.test.mjs
+++ test/src/functions/vGreaterThanOrEqual.test.mjs
@@ -6,57 +6,74 @@ import Validator        from '../../../src/index.mjs';
 
 describe('vGreaterThanOrEqual', () => {
     it('Should be invalid when no params are passed', () => {
-        const evaluation = new Validator({a: 'greater_than_or_equal:<meta.params>'}).validate({a: 10});
+        const evaluation = new Validator({a1: 'greater_than_or_equal:<meta.params>'}).validate({a1: 10});
 
-        assert.equal(evaluation.is_valid, false);
-        assert.deepEqual(evaluation.errors.a, [{msg: 'greater_than_or_equal', params: [undefined]}]);
+        assert.deepEqual(evaluation, {
+            is_valid: false,
+            count: 1,
+            errors: {
+                a1: [{msg: 'greater_than_or_equal', params: [undefined]}],
+            },
+        });
     });
 
     it('Should be invalid when non-numeric params are passed as part of params', () => {
         for (const el of ['hello', true, false]) {
             const evaluation = new Validator({a: 'greater_than_or_equal:<meta>'}).validate({a: 10, meta: el});
-            assert.equal(evaluation.is_valid, false);
-            assert.deepEqual(evaluation.errors.a, [{msg: 'greater_than_or_equal', params: [el]}]);
+            assert.deepEqual(evaluation, {
+                is_valid: false,
+                count: 1,
+                errors: {
+                    a: [{msg: 'greater_than_or_equal', params: [el]}],
+                },
+            });
         }
     });
 
     it('Should be invalid when non-numeric params are passed as part of rule', () => {
         for (const el of ['hello', true, false]) {
             const evaluation = new Validator({a: `greater_than_or_equal:${el}`}).validate({a: 10});
-            assert.equal(evaluation.is_valid, false);
-            assert.deepEqual(evaluation.errors.a, [{msg: 'greater_than_or_equal', params: [`${el}`]}]);
+            assert.deepEqual(evaluation, {
+                is_valid: false,
+                count: 1,
+                errors: {
+                    a: [{msg: 'greater_than_or_equal', params: [`${el}`]}],
+                },
+            });
         }
     });
 
     it('Should return a correct error message when invalid', () => {
-        const evaluation = new Validator({a: 'greater_than_or_equal:10'}).validate({a: 5});
-        assert.equal(evaluation.is_valid, false);
-        assert.deepEqual(evaluation.errors.a, [{msg: 'greater_than_or_equal', params: ['10']}]);
+        const evaluation = new Validator({a1: 'greater_than_or_equal:10'}).validate({a1: 5});
+        assert.deepEqual(evaluation, {
+            is_valid: false,
+            count: 1,
+            errors: {
+                a1: [{msg: 'greater_than_or_equal', params: ['10']}],
+            },
+        });
     });
 
     it('Should allow a string as an acceptable value', () => {
-        const evaluation = new Validator({a: 'greater_than_or_equal:4'}).validate({a: 'hello'});
-        assert.ok(evaluation.is_valid);
+        assert.ok(new Validator({a: 'greater_than_or_equal:4'}).check({a: 'hello'}));
     });
 
     it('Should allow an array as an acceptable value', () => {
-        const evaluation = new Validator({a: 'greater_than_or_equal:5'}).validate({a: ['foo', 'bar', 'bar', 'foo', 'hello', 'world']});
-        assert.ok(evaluation.is_valid);
+        assert.ok(new Validator({a: 'greater_than_or_equal:5'}).check({a: ['foo', 'bar', 'bar', 'foo', 'hello', 'world']}));
     });
 
     it('Should allow a numerical value as an acceptable value', () => {
-        const evaluation = new Validator({a: 'greater_than_or_equal:10'}).validate({a: 15});
-        assert.ok(evaluation.is_valid);
+        assert.ok(new Validator({a: 'greater_than_or_equal:10'}).check({a: 15}));
     });
 
     it('Should disallow NaN values', () => {
-        const evaluation = new Validator({a: 'greater_than_or_equal:10'}).validate({a: Number.NaN});
-        assert.equal(evaluation.is_valid, false);
-    });
-
-    it('Should only allow numerical values as parameter in the rule', () => {
-        const evaluation = new Validator({a: 'greater_than_or_equal:foo'}).validate({a: 5});
-        assert.equal(evaluation.is_valid, false);
+        assert.deepEqual(new Validator({a: 'greater_than_or_equal:10'}).validate({a: Number.NaN}), {
+            is_valid: false,
+            count: 1,
+            errors: {
+                a: [{msg: 'greater_than_or_equal', params: ['10']}],
+            },
+        });
     });
 
     it('Should not allow other types as values', () => {
@@ -84,75 +101,107 @@ describe('vGreaterThanOrEqual', () => {
 
         const evaluation = new Validator(rules).validate(typechecks);
 
-        assert.equal(evaluation.is_valid, false);
-
-        valid_keys.forEach(key => assert.deepEqual(evaluation.errors[key], []));
-        invalid_keys.forEach(key => assert.deepEqual(evaluation.errors[key], [{msg:'greater_than_or_equal', params: ['2']}]));
+        assert.deepEqual(evaluation, {
+            is_valid: false,
+            count: 6,
+            errors: {
+                d1: [{msg:'greater_than_or_equal', params: ['2']}],
+                e1: [{msg:'greater_than_or_equal', params: ['2']}],
+                f1: [{msg:'greater_than_or_equal', params: ['2']}],
+                g1: [{msg:'greater_than_or_equal', params: ['2']}],
+                h1: [{msg:'greater_than_or_equal', params: ['2']}],
+                m1: [{msg:'greater_than_or_equal', params: ['2']}],
+            },
+        });
     });
 
     describe('String', () => {
         it('Should validate a string whose length is greater than the provided parameter as valid', () => {
-            const evaluation = new Validator({a: 'greater_than_or_equal:10'}).validate({a: 'The fox leaped over the fence'});
-            assert.ok(evaluation.is_valid);
+            assert.ok(new Validator({a: 'greater_than_or_equal:10'}).check({a: 'The fox leaped over the fence'}));
         });
 
         it('Should validate a string whose length is equal to the provided parameter as valid', () => {
-            const evaluation = new Validator({a: 'greater_than_or_equal:11'}).validate({a: 'hello world'});
-            assert.ok(evaluation.is_valid);
+            assert.ok(new Validator({a: 'greater_than_or_equal:11'}).check({a: 'hello world'}));
         });
 
         it('Should validate a string whose length is smaller than the provided parameter as invalid', () => {
-            const evaluation = new Validator({a: 'greater_than_or_equal:8'}).validate({a: 'Frantic'});
-            assert.equal(evaluation.is_valid, false);
+            assert.deepEqual(
+                new Validator({a: 'greater_than_or_equal:8'}).validate({a: 'Frantic'}),
+                {
+                    is_valid: false,
+                    count: 1,
+                    errors: {
+                        a: [{msg: 'greater_than_or_equal', params: ['8']}],
+                    },
+                }
+            );
         });
     });
 
     describe('Array', () => {
         it('Should validate an array whose length is greater than the provided parameter as valid', () => {
-            const evaluation = new Validator({a: 'greater_than_or_equal:5'}).validate({a: [1,2,3,4,5]});
-            assert.ok(evaluation.is_valid);
+            assert.ok(new Validator({a: 'greater_than_or_equal:5'}).check({a: [1,2,3,4,5]}));
         });
 
         it('Should validate an array whose length is equal to the provided parameter as valid', () => {
-            const evaluation = new Validator({a: 'greater_than_or_equal:2'}).validate({a: ['foo', 'bar']});
-            assert.ok(evaluation.is_valid);
+            assert.ok(new Validator({a: 'greater_than_or_equal:2'}).check({a: ['foo', 'bar']}));
         });
 
         it('Should validate an array whose length is smaller than the provided parameter as invalid', () => {
-            const evaluation = new Validator({a: 'greater_than_or_equal:3'}).validate({a: ['foo']});
-            assert.equal(evaluation.is_valid, false);
+            assert.deepEqual(
+                new Validator({a: 'greater_than_or_equal:3'}).validate({a: ['foo']}),
+                {
+                    is_valid: false,
+                    count: 1,
+                    errors: {
+                        a: [{msg: 'greater_than_or_equal', params: ['3']}],
+                    },
+                }
+            );
         });
     });
 
     describe('Number', () => {
         it('Should validate a number greater than the provided parameter as valid', () => {
-            const evaluation = new Validator({a: 'greater_than_or_equal:1000'}).validate({a: 10425});
-            assert.ok(evaluation.is_valid);
+            assert.ok(new Validator({a: 'greater_than_or_equal:1000'}).check({a: 10425}));
         });
 
         it('Should validate a number equal to the provided parameter as valid', () => {
-            const evaluation = new Validator({a: 'greater_than_or_equal:42'}).validate({a: 42});
-            assert.ok(evaluation.is_valid);
+            assert.ok(new Validator({a: 'greater_than_or_equal:42'}).check({a: 42}));
         });
 
         it('Should validate a number smaller than the provided parameter as invalid', () => {
-            const evaluation = new Validator({a: 'greater_than_or_equal:502'}).validate({a: 59});
-            assert.equal(evaluation.is_valid, false);
+            assert.deepEqual(
+                new Validator({a: 'greater_than_or_equal:502'}).validate({a: 59}),
+                {
+                    is_valid: false,
+                    count: 1,
+                    errors: {
+                        a: [{msg: 'greater_than_or_equal', params: ['502']}],
+                    },
+                }
+            );
         });
 
         it('Should validate negative number ranges that are above the provided parameter as valid', () => {
-            const evaluation = new Validator({a: 'greater_than_or_equal:-500'}).validate({a: -245});
-            assert.ok(evaluation.is_valid);
+            assert.ok(new Validator({a: 'greater_than_or_equal:-500'}).check({a: -245}));
         });
 
         it('Should validate negative number ranges that are below the provided parameter as invalid', () => {
-            const evaluation = new Validator({a: 'greater_than_or_equal:-20'}).validate({a: -45});
-            assert.equal(evaluation.is_valid, false);
+            assert.deepEqual(
+                new Validator({a: 'greater_than_or_equal:-20'}).validate({a: -45}),
+                {
+                    is_valid: false,
+                    count: 1,
+                    errors: {
+                        a: [{msg: 'greater_than_or_equal', params: ['-20']}],
+                    },
+                }
+            );
         });
 
         it('Should validate negative number ranges that are equal to the provided parameter as valid', () => {
-            const evaluation = new Validator({a: 'greater_than_or_equal:-999'}).validate({a: -999});
-            assert.ok(evaluation.is_valid);
+            assert.ok(new Validator({a: 'greater_than_or_equal:-999'}).check({a: -999}));
         });
     });
 });
